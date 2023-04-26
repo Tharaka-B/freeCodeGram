7c54679d995f3100fd3a3d9238798072eaec5fa4
Tharaka Bandara <tharakabandara@seven-sys.co.jp>
2023-04-26 09:22:53 +0900

    first commit

---
+++ app/Models/Profile.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Profile extends Model
+{
+    use HasFactory;
+
+    protected $guarded = [];
+
+    public function profileImage()
+    {
+        $imagePath = ($this->image) ?  $this->image : 'profile/1Ql4yD0cRmaKf2AIGw9vr8nHIAwcI1tuk4wdnU1C.png';
+        return '/storage/' . $imagePath;
+    }
+
+    public function followers()
+    {
+        return $this->belongsToMany(User::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
